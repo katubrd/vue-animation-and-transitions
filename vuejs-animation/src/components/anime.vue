84cc2636e3c7763ee82fcb30a493be511a77f1d1
Katu_brd <katuthings@gmail.com>
2018-03-15 13:47:41 +0100

    Based on vue-cli simple toggle animation

---
+++ vuejs-animation/src/components/anime.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div>
+    <div v-if="visible">
+      <h3 class="text">Woooooo!!!<br>I'm a ghost!!!</h3>
+    </div>
+    <button @click="toggle">Toggle</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      visible: true
+    }
+  },
+  methods: {
+    toggle: function() {
+      this.visible = !this.visible
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
