1540baa37dcb42fb9395a9247b52b6e10453405c
Katu_brd <katuthings@gmail.com>
2018-03-15 14:46:51 +0100

    v-if v-else and transition exercise

--- vuejs-animation/src/components/anime.vue
+++ vuejs-animation/src/components/anime.vue
@@ -1,9 +1,16 @@
 <template lang="html">
   <div>
-    <div v-if="visible">
-      <h3 class="text">Woooooo!!!<br>I'm a ghost!!!</h3>
+    <div>
+      <transition name="bounce" appear mode="in-out">
+        <div v-if="visible" key="visible">
+          <h3 class="text">Woooooo!!!<br>I'm a ghost!!!</h3>
+        </div>
+        <div v-else="visible" key="invisible">
+          <h3 class="text">Woooooo!!!<br>I'm a ghost to!!!</h3>
+        </div>
+      </transition>
+      <button class="toggle-btn" @click="toggle">Toggle</button>
     </div>
-    <button @click="toggle">Toggle</button>
   </div>
 </template>
 
@@ -23,4 +30,70 @@ export default {
 </script>
 
 <style lang="css">
+  .toggle-btn {
+    position: absolute;
+    top: 25vh;
+    right: 48vw;
+  }
+  .bounce {
+    top: 29vh;
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s, transform .5s;
+  }
+  .fade-enter, .fade-leave-active {
+    opacity: 0;
+    transform: translateY(-3vh);
+  }
+  .bounce-enter-active {
+    animation: bounce-in .5s;
+  }
+  .bounce-leave-active {
+    animation: bounce-out .5s;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transfrom: scale(1);
+    }
+  }
+  @keyframes bounce-out {
+    0% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </style>
